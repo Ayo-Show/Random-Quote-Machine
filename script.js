d848711bcd4d44ef4c88e06e85ed0c264ae0f24a
Ayo-Show <sowunmi30@gmail.com>
2022-02-10 11:07:13 -0500

    added a loading indicator

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-const newQuoteButton = document.querySelector('#js-new-quote');
+const newQuoteButton = document.querySelector("#js-new-quote");
+const spinner = document.querySelector('#js-spinner');
 
+newQuoteButton.addEventListener("click", getQuote);
 
-newQuoteButton.addEventListener('click', getQuote);
+const endpoint = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
 
-function getQuote() {
-  console.log("quote button was clicked");
+async function getQuote() {
+  // remove the "hidden" class on the spinner
+  spinner.classList.remove('hidden');
+  // disable the quote button
+  newQuoteButton.disabled = true;
+
+  try {
+    const response = await fetch(endpoint)
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    const json = await response.json();
+    displayQuote(json.message);
+  } catch {
+    alert('Failed to fetch new quote');
+  } finally {
+    // enable the quote button
+    newQuoteButton.disabled = false;
+    // add the "hidden" class back again
+    spinner.classList.add('hidden');
+  }
 }
 
-
+function displayQuote(quote) {
+  const quoteText = document.querySelector('#js-quote-text');
+  quoteText.textContent = quote;
+}
